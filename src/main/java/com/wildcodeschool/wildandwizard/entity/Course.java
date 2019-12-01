56505d41c9bcac24c7c08418ad31117bff2971ba
52-28 <aymeric.feillault@gmail.com>
2019-12-01 14:51:17 +0100

    challenge resolution

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
@@ -1,9 +1,13 @@
 package com.wildcodeschool.wildandwizard.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Course {
@@ -13,6 +17,9 @@ public class Course {
     private Long id;
     private String name;
 
+    @ManyToMany(mappedBy = "courses")
+    private List<Wizard> wizards = new ArrayList<>();
+    
     public Course() {
     }
 
